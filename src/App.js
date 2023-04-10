79e0cfed81de6c5039d0d47045ca58c99d10f830
JEEVAABI <jeevaabishake@gmail.com>
2023-04-10 14:05:24 +0530

    Add.API implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {BrowserRouter as Router,Link,Routes,Route } from "react-router-dom";
 import './App.css';
 import CV from "./components/CvCompoenent/CV";
-import Calcula from "./components/CalculatorComponent/Calcula";
-import GetComponent from "./components/GetComponent/GetComponent";
+import Calcula from './components/CalculatorComponent/Calcula';
+import Getcomponent from './components/GetComponent/Getcomponent';
 
 function App() {
   return (
@@ -11,13 +11,13 @@ function App() {
       
           <Link to="/cv">Portfolio</Link>
           <Link to="/calcula">Calculator</Link>
-          <Link to="/GetCom>">Rick&Morty</Link>
+          <Link to="/GetCom">Rick&Morty</Link>
         
 
       <Routes>
         <Route exact path='/cv' element={<CV/>}></Route>
         <Route exact path='/calcula' element={<Calcula/>}></Route>
-        <Route exact path='/GetCom' element={<GetComponent/>}></Route>
+        <Route exact path='/GetCom' element={<Getcomponent/>}></Route>
 
         
       </Routes>
--- src/components/CvCompoenent/CV.jsx
+++ src/components/CvCompoenent/CV.jsx
@@ -3,21 +3,56 @@ import React, { Component } from 'react'
 export class CV extends Component {
   render() {
     return (
-      <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm jeeva.
-            <br className="hidden lg:inline-block" />I am a student
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            currently working on my react project called react-routers-forms-axsios
-          </p>
-          
-        </div>
-        
-      </div>
-    </section>
+      <React.Fragment>
+      {/* <header>
+		    <div>
+			    <h1>Jeeva Abishake</h1>
+			    <h2>Full Stack Developer</h2>
+          </div>
+          <div>
+		       <img src="./src/components/CvCompoenent/img/profile.jpg" alt="Profile Picture"/>
+           </div>
+	
+	    </header> */}
+	    <main>
+		    <section>
+			    <h3>Contact</h3>
+			      <ul>
+				      <li><label>Email:</label> jeevaabishake@gmail.com</li>
+				      <li><label>Phone:</label> 805666***</li>
+				      <li><label>Address:</label> chennai</li>
+			      </ul>
+		    </section>
+		    <section>
+			    <h3>Skills</h3>
+			    <ul>
+				    <li>HTML</li>
+				    <li>CSS</li>
+				    <li>JavaScript</li>
+				    <li>PHP</li>
+				    <li>MySQL</li>
+			    </ul>
+		    </section>
+		    <section>
+			    <h3>Experience</h3>
+			    <p><strong>Full Stack Developer</strong>, --- Company</p>
+			    <p><em>June 2024 - Present</em></p>
+			    <ul>
+				    <li>Developed and maintained company website using HTML, CSS, and JavaScript</li>
+				    <li>Implemented responsive design for mobile devices using CSS media queries</li>
+				    <li>Created and managed MySQL databases for the website</li>
+			    </ul>
+			  </section>
+		    <section>
+			    <h3>Education</h3>
+			    <p><strong>Bachelor of Technology in Artificial Intelligence and machine learning</strong>, Saveetha Engineering College</p>
+			    <p><em>November 2021 - june 2024</em></p>
+			    <p>Courses included: Artificial Intelligence, Data Structures, Algorithms, Web Development, Database Management, Data Science, Machine Learning</p>
+		    </section>
+	    </main>
+
+     </React.Fragment>
+      
     )
   }
 }
